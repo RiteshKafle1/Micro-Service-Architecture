aaa2ac8cc8780e6969926ccaeee5e4efad6d21db
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-11-15 11:02:59 +0545

    connection of database using docker image and container

---
+++ UserService/config/db.config.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDb = async (dbUri) => {
+  try {
+    await mongoose.connect(dbUri);
+    console.log("Connected to Database");
+  } catch (error) {
+    console.log("Error in connecting to a db");
+  }
+};
+export default connectDb;
--- UserService/index.js
+++ UserService/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import "dotenv/config";
+import connectDb from "./config/db.config.js";
 const app = express();
-const PORT = 4500;
 
+
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:4500`);
+  connectDb(process.env.MONGODB_URI);
+  console.log(`UserService is running on http://localhost:${PORT}`);
 });
