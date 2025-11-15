5d05c500a0cc28b5f4abf9621acadbc25a097469
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-11-15 15:47:41 +0545

    issues fixed

--- UserService/config/db.config.js
+++ UserService/config/db.config.js
@@ -5,7 +5,7 @@ const connectDb = async (dbUri) => {
     await mongoose.connect(dbUri);
     console.log("Connected to Database");
   } catch (error) {
-    console.log("Error in connecting to a db");
+    console.log("Error in connecting to a db",error);
   }
 };
 export default connectDb;
